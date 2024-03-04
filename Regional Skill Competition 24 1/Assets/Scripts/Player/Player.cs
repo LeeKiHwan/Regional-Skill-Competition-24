d78e0b32ffedd565703229b3a304a5a36e27fa4b
LeeKiHwan <s22049@gsm.hs.kr>
2024-03-04 20:23:52 +0900

    플레이어 제작

---
+++ Regional Skill Competition 24 1/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed;
+    public float rotSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        float z = Input.GetAxis("Vertical") * speed;
+        float yRot = Input.GetAxis("Horizontal") * rotSpeed;
+
+        rb.AddForce(transform.forward * z, ForceMode.Acceleration);
+
+        transform.Rotate(transform.up, yRot);
+    }
+}
---
+++ Regional Skill Competition 24 1/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform lookTarget;
+    public Transform positionTarget;
+    public float followSpeed;
+
+    private void Update()
+    {
+        transform.LookAt(lookTarget.position);
+        transform.position = Vector3.Lerp(transform.position, positionTarget.position, followSpeed * Time.deltaTime);
+    }
+}
+    
