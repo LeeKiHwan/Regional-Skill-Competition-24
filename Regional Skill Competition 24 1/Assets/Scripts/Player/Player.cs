cd05035c373070aae399ef9581df33f14f5ba69e
LeeKiHwan <s22049@gsm.hs.kr>
2024-03-05 08:46:07 +0900

    플레이어 이동 수정

--- Regional Skill Competition 24 1/Assets/Scripts/Player/Player.cs
+++ Regional Skill Competition 24 1/Assets/Scripts/Player/Player.cs
@@ -22,10 +22,9 @@ public class Player : MonoBehaviour
     public void Move()
     {
         float z = Input.GetAxis("Vertical") * speed;
-        float yRot = Input.GetAxis("Horizontal") * rotSpeed;
+        float yRot = Input.GetAxis("Horizontal") * rotSpeed * Input.GetAxis("Vertical");
 
         rb.AddForce(transform.forward * z, ForceMode.Acceleration);
-
         transform.Rotate(transform.up, yRot);
     }
 }
