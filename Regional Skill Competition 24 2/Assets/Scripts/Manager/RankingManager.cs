0d2bd8daeaa2e2678b7342d80ca0233f583d06ba
LeeKiHwan <s22049@gsm.hs.kr>
2024-03-25 14:43:50 +0900

    기본 클래스 생성

---
+++ Regional Skill Competition 24 2/Assets/Scripts/Manager/RankingManager.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RankingManager : MonoBehaviour
+{
+    
+}
